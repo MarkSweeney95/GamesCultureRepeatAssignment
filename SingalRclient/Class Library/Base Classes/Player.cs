9fb7ce1bdac947ec0129e8a5e75686b5c6cd0f44
MarkSweeney380 <markgsweeney38@gmail.com>
2016-07-22 20:21:44 +0100

    frist commit
    base classes done for character and weapons

---
+++ SingalRclient/Class Library/Base Classes/Player.cs
@@ -0,0 +1,27 @@
+﻿using Class_Library.Ship;
+using Class_Library.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class_Library.Base_Classes
+{
+    public class Player : Sprite
+    {
+        public string UserName; //username which appears in the chat
+        public Base_Ship Ship;
+        public Weapon weapon;
+        public bool isActive;
+
+        public Player(string uName, Base_Ship _ship, Weapon _weapon)
+        {
+            UserName = uName;
+            Ship = _ship;
+            Weapon = _weapon;
+            Ship.weapon = Weapon;
+            isActive = true;
+        }
+
+    }
+}
---
+++ SingalRclient/SingalRclient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SingalRclient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SingalRclient")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58e6c22e-ead4-4ef5-84f1-cbf2621f444f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
