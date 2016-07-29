f592ec7d743429046b965707267c15fff5721b72
MarkSweeney380 <markgsweeney38@gmail.com>
2016-07-29 02:00:49 +0100

    THIRD COMMIT
    started player hub

--- SingalRclient/SignalRWebClient/Hub.cs
+++ SingalRclient/SignalRWebClient/Hub.cs
@@ -10,7 +10,16 @@ namespace SignalRWebClient
 
     {
         public string PlayerID;
-        // Add more fields here
+        
+        public string GamerTag;
+        public string UserName;
+        public string FirstName;
+        public string SecondName;
+        public int XP;
+        public string clientID;
+  
+      
+
     }
 
 
@@ -18,12 +27,15 @@ namespace SignalRWebClient
     public static class HubState
 
     {
+        
         public static List<Player> players = new List<Player>()
 
-        { new Player { PlayerID = "player1"  },
+        { new Player { PlayerID = "player1" ,GamerTag="XMARCASX",UserName="deadmpunk",FirstName="m",SecondName="s",XP=100000,clientID="1" },
          new Player { PlayerID = "player2"  },
-         // etc
-        };
+    
+
+        // etc
+    };
 
     }
 
