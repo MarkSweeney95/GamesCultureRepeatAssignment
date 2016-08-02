f8cb0249401a11bf395840417d234721df9a893a
MarkSweeney380 <markgsweeney38@gmail.com>
2016-08-02 23:56:09 +0100

    added more code to the login
    absent for a few days due to prior constrants

--- SingalRclient/SignalRWebClient/Hub.cs
+++ SingalRclient/SignalRWebClient/Hub.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.Ajax.Utilities;
 
 namespace SignalRWebClient
 {
@@ -10,24 +11,70 @@ namespace SignalRWebClient
 
     {
         public string PlayerID;
-        
+
         public string GamerTag;
         public string UserName;
         public string FirstName;
         public string SecondName;
         public int XP;
         public string clientID;
-  
-      
+        public bool playing;
 
-    }
+        public static List<Player> players = new List<Player>()
+
+        { new Player { PlayerID = "player1" ,GamerTag="XMARCASX",UserName="deadmpunk",FirstName="m",SecondName="s",XP=100000,clientID="1" },
+         new Player { PlayerID = "player2"  },
+
+        public Player join(string username)
+        {
+            if (!playing)
+            {
+                Player p = players.FirstOrDefault(pl => pl.UserName == username);
+                if (p != null && !joined.Contains(p)) // Valid player
+                {
+                    p.clientID = Context.ConnectionId;
+                    joined.Add(p);
+                    if (joined.Count() > 1)
+                    {
+                        Clients.All.play(joined); // Note clients must subscribe to this event
+                        playing = true;
+                    }
+                    return p;
+                }
+                else return null;
+            }
+            else return null;
+        }
+
+        public void joinUp(string username)
+        {
+            if (!playing)
+            {
+                Player p = players.FirstOrDefault(pl => pl.UserName == username);
+                if (p != null && !joined.Contains(p)) // Valid player
+                {
+                    p.clientID = Context.ConnectionId;
+                    joined.Add(p);
+                    Clients.Caller.joined(p);
+                    if (joined.Count() > 1)
+                    {
+                        Clients.All.play(joined); // Note clients must subscribe to this event
+                        playing = true;
+                    }
+                }
+            }
 
 
-    // Note this 
-    public static class HubState
+        }
+
+
+
+
+        // Note this 
+        public static class HubState
 
     {
-        
+
         public static List<Player> players = new List<Player>()
 
         { new Player { PlayerID = "player1" ,GamerTag="XMARCASX",UserName="deadmpunk",FirstName="m",SecondName="s",XP=100000,clientID="1" },
@@ -36,6 +83,8 @@ namespace SignalRWebClient
 
         // etc
     };
+
+
 
     }
 
